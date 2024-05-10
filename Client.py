3a69b57977e93b936dd6788283997fa8b5b7f4a2
Mohammed Kadri <33161436+mohammed-kadri@users.noreply.github.com>
2024-05-10 17:53:34 +0100

    Fixing bugs and adding private messaging feature.

--- Client.py
+++ Client.py
@@ -5,80 +5,72 @@ import tkinter as tk
 from tkinter import simpledialog
 import tkinter.scrolledtext
 import pickle
+from PIL import ImageTk, Image
+import customtkinter
 import time
 
-HOST = socket.gethostname()
+import tkinter as tk
+
+
+
+HOST = "192.168.1.106"
 PORT = 9999
+
+
 
 class Client:
     def __init__(self, host, port):
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
-
+        self.current_disccusion_index = 0
         msg = tk.Tk()
         msg.withdraw()
 
-        self.testarray = []
-        self.a = 3
-        self.testarray.append(self.a)
 
         self.ChatRooms = []
-
-
-        print(f"a: {self.a}")
-        print(f"array: {self.testarray[0]}")
-        print("------------")
-        self.a = 8
-        print(f"a: {self.a}")
-        print(f"array: {self.testarray[0]}")
-
+        self.nickname = ''
         self.nickname = simpledialog.askstring("Login", "Enter your nickname", parent=msg)
-
         self.gui_done = False
         self.running = True
-
         gui_thread = threading.Thread(target=self.gui_loop)
-        recieve_thread = threading.Thread(target=self.receive)
+        recievread = threading.Thread(target=self.receive)
         gui_thread.start()
         recieve_thread.start()
 
     def center_window(self, window, width, height):
         screen_width = window.winfo_screenwidth()
         screen_height = window.winfo_screenheight()
-
         x = (screen_width - width) // 2
-        y = (screen_height - height) // 2
-
+        y = (screen_height - height) // 2e_th
         window.geometry(f"{width}x{height}+{x}+{y}")
 
     def gui_loop(self):
         self.win = tk.Tk()
         self.win.configure(background='white')
         self.win.title(self.nickname)
-        window_width = 600
-        window_height = 400
+        window_width = 650
+        window_height = 600
         self.center_window(self.win, window_width, window_height)
 
-        # Create the main frame
         main_frame = tk.Frame(self.win, bg='white')
         main_frame.pack(fill='both', expand=True)
 
-        # Create the left frame for displaying connected users
+        self.win.resizable(False, False)
+
         self.users_frame = tk.Frame(main_frame, bg='lightgray', padx=10, pady=10)
         self.users_frame.pack(side='left', fill='both')
 
         users_label = tk.Label(self.users_frame, text='Connected Users:', bg='lightgray', font=('Arial', 14, 'bold'))
-        users_label.pack(pady=5)
+        users_label.pack(pady=0)
 
-        # Create a frame for the public chat button
         public_chat_frame = tk.Frame(main_frame, bg='lightgray', padx=10, pady=10)
         public_chat_frame.pack(side='left', fill='both')
 
-        self.public_chat_button = tk.Button(public_chat_frame, text='Public', command=lambda: self.switch_conversation(0))
+        self.public_chat_button = tk.Button(public_chat_frame, text='Public',
+                                            command=lambda: self.switch_conversation(0))
         self.public_chat_button.pack(pady=5)
 
-        # Create the right frame for the chat area
         self.right_frame = tk.Frame(main_frame, bg='white', padx=10, pady=10)
         self.right_frame.pack(side='left', fill='both', expand=True)
 
@@ -107,20 +99,23 @@ class Client:
         )
 
         self.gui_done = True
-        # self.ChatRooms[0]["messages_area"].pack_forget()
         self.win.protocol('WM_DELETE_WINDOW', self.stop)
         self.win.mainloop()
 
-    def switch_conversation(self, order):
+    def switch_conversation(self, order, old_textarea=""):
+        self.current_disccusion_index = order
         if self.gui_done:
             for chatroom in self.ChatRooms:
                 chatroom["messages_area"].pack_forget()
             self.ChatRooms[order]["messages_area"].pack()
+            try:
+                old_textarea.pack_forget()
+            except:
+                pass
         print(self.ChatRooms)
 
-
     def update_users_list(self, nicknames):
-       while True:
+        while True:
             try:
                 print(nicknames)
                 for widget in self.users_frame.winfo_children():
@@ -128,46 +123,51 @@ class Client:
                 for nickname in nicknames:
                     user_frame = tk.Frame(self.users_frame, bg='lightgray')
                     user_frame.pack(fill='x', padx=5, pady=2)
-                    user_label = tk.Label(user_frame, text=nickname, font=('Arial', 12), bg='lightgray')
+                    user_label = tk.Label(user_frame, text=nickname, bg='lightgray')
                     user_label.pack(side='left')
-
-
-
                     if nickname != self.nickname:
-                        message_button = tk.Button(user_frame, text='Message', command=lambda n=nickname: self.send_message_to(n))
+                        message_button = tk.Button(user_frame, text='Message',
+                                                   command=lambda n=nickname: self.switch_conversation(
+                                                       nicknames.index(n) + 1))
                         message_button.pack(side='left')
                 break
             except:
                 pass
-       textarea_created = False
-       while textarea_created==False :
-           try:
-               for nickname in nicknames:
-                   print(f"im what u want: {nicknames}")
+        textarea_created = False
+        while textarea_created == False:
+            try:
+                for nickname in nicknames:
+                    if not any(chatroom['key'] == nickname for chatroom in self.ChatRooms):
+                        self.new_text_area = tkinter.scrolledtext.ScrolledText(self.right_frame)
+                        self.new_text_area.pack(padx=20, pady=5)
+                        self.new_text_area.config(state='disabled')
+                        self.new_text_area.pack_forget()
+                        new_chatroom = {
+                            "key": nickname,
+                            "messages_area": self.new_text_area
+                        }
+                        self.ChatRooms.append(new_chatroom)
+                    temp_chatroom = self.ChatRooms
+                    self.ChatRooms = [chatroom for chatroom in self.ChatRooms if
+                                      chatroom['key'] == 'PUBLIC' or chatroom['key'] in nicknames]
+                    if not temp_chatroom == self.ChatRooms:
+                        missing_textarea = self.get_missing_chatroom(temp_chatroom, self.ChatRooms)["messages_area"]
+                        print(self.get_missing_chatroom(temp_chatroom, self.ChatRooms)["key"])
+                        self.switch_conversation(0, missing_textarea)
+                    textarea_created = True
 
-                   if not any(chatroom['key'] == nickname for chatroom in self.ChatRooms):
+            except Exception as e:
+                print(e)
 
-                       self.new_text_area = tkinter.scrolledtext.ScrolledText(self.right_frame)
-                       self.new_text_area.pack(padx=20, pady=5)
-                       self.new_text_area.config(state='disabled')
-                       self.new_text_area.pack_forget()
-                       new_chatroom = {
-                           "key": nickname,
-                           "messages_area": self.new_text_area
-                       }
-                       self.ChatRooms.append(new_chatroom)
-                       print("heloooooooooooo")
+    def get_missing_chatroom(self, array1, array2):
+        for dictionary in array1:
+            key = dictionary.get("key")
+            if key not in [d.get("key") for d in array2]:
+                return dictionary
+        return None
 
-                   self.ChatRooms = [chatroom for chatroom in self.ChatRooms if
-                                     chatroom['key'] == 'PUBLIC' or chatroom['key'] in nicknames]
-
-                   textarea_created = True
-
-           except Exception as e:
-               print(e)
-
+    # If no missing key is found
     def send_message_to(self, nickname):
-        # Your code to handle sending a message to a specific user goes here
         pass
 
     def send_message(self, event):
@@ -176,7 +176,8 @@ class Client:
 
     def write(self):
         written_text = self.input_area.get('1.0', 'end')
-        message = {"key": "MESSAGE", "from": self.nickname, "to": "ALL", "text": written_text.strip()}
+        to = self.ChatRooms[self.current_disccusion_index]["key"]
+        message = {"key": "MESSAGE", "from": self.nickname, "to": to, "text": written_text.strip()}
         message_bytes = pickle.dumps(message)
         if message["text"]:
             self.sock.send(message_bytes)
@@ -199,52 +200,52 @@ class Client:
                     self.sock.send(nick)
                 elif message["key"] == "MESSAGE":
                     if self.gui_done:
-                        message_text = message["text"]
-                        self.text_area.config(state=tk.NORMAL)
-                        if self.nickname == message["from"]:
-                            self.text_area.insert(tk.END, 'Me: ' + message_text + '\n')
+                        if message["to"] == "PUBLIC":
+                            message_text = message["text"]
+                            self.active_text_area = self.ChatRooms[0]["messages_area"]
+                            self.active_text_area.config(state=tk.NORMAL)
+                            if self.nickname == message["from"]:
+                                self.active_text_area.insert(tk.END, 'Me: ' + message_text + '\n')
+                            else:
+                                self.active_text_area.insert(tk.END, message["from"] + ': ' + message_text + '\n')
+                            self.active_text_area.yview(tk.END)
+                            self.active_text_area.config(state=tk.DISABLED)
+
+                        elif self.nickname == message["from"]:
+                            message_text = message["text"]
+                            to_disccusion = 0
+                            for index, room in enumerate(self.ChatRooms):
+                                if room["key"] == message["to"]:
+                                    to_disccusion = index
+                                    break
+                            self.active_text_area = self.ChatRooms[to_disccusion]["messages_area"]
+                            self.active_text_area.config(state=tk.NORMAL)
+                            if self.nickname == message["from"]:
+                                self.active_text_area.insert(tk.END, 'Me: ' + message_text + '\n')
+                            else:
+                                self.active_text_area.insert(tk.END, message["from"] + ': ' + message_text + '\n')
+                            self.active_text_area.yview(tk.END)
+                            self.active_text_area.config(state=tk.DISABLED)
+
                         else:
-                            self.text_area.insert(tk.END, message["from"] + ': ' + message_text + '\n')
-                        self.text_area.yview(tk.END)
-                        self.text_area.config(state=tk.DISABLED)
+                            message_text = message["text"]
+                            to_disccusion = 0
+                            for index, room in enumerate(self.ChatRooms):
+                                if room["key"] == message["from"]:
+                                    to_disccusion = index
+                                    break
+                            self.active_text_area = self.ChatRooms[to_disccusion]["messages_area"]
+                            self.active_text_area.config(state=tk.NORMAL)
+                            if self.nickname == message["from"]:
+                                self.active_text_area.insert(tk.END, 'Me: ' + message_text + '\n')
+                            else:
+                                self.active_text_area.insert(tk.END, message["from"] + ': ' + message_text + '\n')
+                            self.active_text_area.yview(tk.END)
+                            self.active_text_area.config(state=tk.DISABLED)
+                            pass
                 elif message["key"] == 'NICKNAMESLIST':
                     nicknames = pickle.loads(message["nicknames"])
                     self.update_users_list(nicknames)
-
-                    # while True:
-                    #     found_new_nickname = True
-                    #     for chatroom in self.ChatRooms:
-                    #         for nickname in nicknames:
-                    #             if nickname == chatroom["key"]:
-                    #                 found_new_nickname = False
-                    #
-                    #     if found_new_nickname:
-                    #         while True:
-                    #             try:
-                    #                 self.new_text_area = tkinter.scrolledtext.ScrolledText(right_frame)
-                    #                 self.new_text_area.pack(padx=20, pady=5)
-                    #                 self.new_text_area.config(state='disabled')
-                    #                 self.new_text_area.pack_forget()
-                    #                 new_chatroom = {
-                    #                     "key": nickname,
-                    #                     "messages_area": self.new_text_area
-                    #                 }
-                    #                 self.ChatRooms.append(self.new_chatroom)
-                    #                 break
-                    #             except:
-                    #                 pass
-
-                    # if self.gui_done:
-                    #     self.new_text_area = tkinter.scrolledtext.ScrolledText(right_frame)
-                    #     self.new_text_area.pack(padx=20, pady=5)
-                    #     self.new_text_area.config(state='disabled')
-                    #     self.new_text_area.pack_forget()
-                    #     new_chatroom = {
-                    #         "key": nickname,
-                    #         "messages_area": self.new_text_area
-                    #     }
-                    #     self.ChatRooms.append(self.new_chatroom)
-                    #     print(self.ChatRooms)
             except ConnectionAbortedError:
                 break
 
--- Server.py
+++ Server.py
@@ -2,47 +2,41 @@ import socket
 import threading
 import pickle
 
-HOST = socket.gethostname()
+HOST = "192.168.1.106"
 PORT = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 server.bind((HOST, PORT))
 server.listen(5)
-
 clients = []
 nicknames = []
 
 def send(message):
     message_bytes = pickle.dumps(message)
     to = message["to"]
-
-    if to == "ALL":
-
+    if to == "PUBLIC":
         for client, nickname in zip(clients, nicknames):
             if nickname == message["from"]:
                 message["text"] = "Me: " + message["text"]
             print(message["from"])
             client.send(message_bytes)
+    else:
+        clients[nicknames.index(message["from"])].send(message_bytes)
+        clients[nicknames.index(message["to"])].send(message_bytes)
 
 def handle(client):
     while True:
         try:
             message_bytes = client.recv(1024)
             if not message_bytes:
-                # Client disconnected
                 break
-
             message = pickle.loads(message_bytes)
             send(message)
-
         except Exception as e:
-            # Handle any exceptions that occur during message handling
             print(f"Error handling message from client: {e}")
             break
 
-    # Client disconnected, remove it from the lists
     if client in clients:
         index = clients.index(client)
         clients.remove(client)
@@ -61,24 +55,17 @@ def broadcast_nicknames():
 def receive():
     while True:
         client, address = server.accept()
-        # print(f'Connected by: {str(address)}')
         broadcast_nicknames()
         NICK = {"key": "NICK"}
         NICK = pickle.dumps(NICK)
         client.send(NICK)
-
         nickname_bytes = client.recv(1024)
         nickname_bytes = pickle.loads(nickname_bytes)
         nickname = nickname_bytes["value"].decode('utf-8')
-
         nicknames.append(nickname)
         clients.append(client)
         broadcast_nicknames()
-
-
-
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-
 print("Server starting...")
 receive()
